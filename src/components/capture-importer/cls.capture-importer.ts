31c9670070ed58d1de0be4055be74cf79989fa0c
Tamius Han <ziga.pirih@hotmail.com>
2026-09-15 21:57:30 +0200

    Implement unified file handling and import logic (wip)

--- src/components/capture-importer/cls.capture-importer.ts
+++ src/components/capture-importer/cls.capture-importer.ts
@@ -106,25 +106,70 @@ export class CaptureImporter extends HTMLElement {
   private setupEvents() {
     this.dropzone.addEventListener("click", () => this.folderInput.click());
 
-    this.dropzoneOuter.addEventListener("dragover", (e) => {
-      e.preventDefault();
-      this.dropzone.classList.add("drag");
-    });
-    this.dropzoneOuter.addEventListener("dragleave", () => this.dropzone.classList.remove("drag"));
+    // For the time being, we don't change classes when mouse hovers over global dropzone
+    // this.dropzoneOuter.addEventListener("dragover", (e) => {
+    //   e.preventDefault();
+    //   this.dropzone.classList.add("drag");
+    // });
+    // this.dropzoneOuter.addEventListener("dragleave", () => this.dropzone.classList.remove("drag"));
+
     this.dropzoneOuter.addEventListener("drop", async (e) => {
       e.preventDefault();
       this.dropzone.classList.remove("drag");
       if (!e.dataTransfer) return;
-      this.setStatus("Reading dropped folder...");
+      this.setStatus("[global dropzone] Reading dropped files ...");
       const entries = await collectFromDrop(e.dataTransfer);
       await this.handleFiles(entries);
     });
     this.folderInput.addEventListener("change", async (e) => {
       const files = (e.target as HTMLInputElement).files;
       if (!files) return;
-      this.setStatus("Reading folder...");
+      this.setStatus("[renderdoc folder input] Reading folder...");
       await this.handleFiles(collectFromInput(files));
     });
+    this.dropzone.addEventListener("dragover", (e) => {
+      e.stopPropagation();
+      e.preventDefault();
+      this.dropzone.classList.add("drag");
+    });
+    this.dropzone.addEventListener("dragleave", () => this.dropzone.classList.remove("drag"));
+    this.dropzone.addEventListener("drop", async (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      this.dropzone.classList.remove("drag");
+      if (!e.dataTransfer) return;
+      this.setStatus("[renderdoc dropzone] Reading dropped folder...");
+      const entries = await collectFromDrop(e.dataTransfer);
+      await this.handleFiles(entries);
+    });
+
+    for (const target in this.intelGPADropzones) {
+      const dropzone = this.intelGPADropzones[target as IntelGPADropzone].dropzone;
+      const input = this.intelGPADropzones[target as IntelGPADropzone].input;
+
+      dropzone.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        dropzone.classList.add("drag");
+      });
+      dropzone.addEventListener('dragleave', () => dropzone.classList.remove("drag"));
+
+      dropzone.addEventListener('drop', async (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        if (!e.dataTransfer) return;
+        this.setStatus(`[intel GPA dropzone::${target}] Reading dropped folder...`);
+        const entries = await collectFromDrop(e.dataTransfer);
+        await this.handleFiles(entries, target as IntelGPADropzone);
+      });
+
+      input.addEventListener('change', async (e) => {
+        const files = (e.target as HTMLInputElement).files;
+        if (!files) return;
+        this.setStatus(`[intel GPA folder input::${target}] Reading folder...`);
+        await this.handleFiles(collectFromInput(files), target as IntelGPADropzone);
+      });
+    }
 
     // this.reconstructBtn.addEventListener("click", () => void this.reconstructScene());
     // this.recalculateCorrectionBtn.addEventListener("click", () => this.recalculateTransformCorrection());
@@ -153,8 +198,145 @@ export class CaptureImporter extends HTMLElement {
     this.statusBar.textContent = message;
   }
 
-  private async handleFiles(entries: { path: string; file: File }[]): Promise<void> {
-    if (entries.length === 0) return;
+  /**
+   * Determines if files dropped into the input field represent an export
+   * made with RenderDoc Scene Exporter addon
+   * @param entries The list of file entries to check
+   * @returns A promise that resolves to true if the entries represent a RenderDoc export, false otherwise
+   */
+  private async isRenderDocExport(entries: FileInfo[]): Promise<boolean> {
+    let root = `${entries[0].path.split('/')[0]}/`;
+    let hasManifest = false; let hasPass = false;
+
+    if (root === "manifest.json") {
+      hasManifest = true;
+      root = '';
+    }
+    if (root.startsWith('pass_')) {
+      hasPass = true;
+      root = '';
+    }
+
+    const manifestPath = `${root}manifest.json`;
+    const passPath = `${root}pass_`;
+
+    if (!hasPass) {
+      for (const e of entries) {
+        if (e.path.startsWith(passPath)) {
+          hasPass = true;
+          break;
+        }
+      }
+    }
+    if (!hasManifest) {
+      for (const e of entries) {
+        if (e.path === manifestPath) {
+          hasManifest = true;
+          break;
+        }
+      }
+    }
+
+    return hasManifest && hasPass;
+  }
+
+  /**
+   * Identifies what kind of import the dropped files represent
+   * @param entries The list of file entries to check
+   * @returns A promise that resolves to the identified import type
+   */
+  private async identifyImportType(entries: FileInfo[]): Promise<ImportType> {
+    if (await this.isRenderDocExport(entries)) {
+      return ImportType.RenderDocExport;
+    }
+
+    // if there's three files, and if all three files are .obj, then
+    // we assume it's full IntelGPADropzone import
+    if (entries.length === 3 && entries.every(e => e.path.endsWith('.obj'))) {
+      return ImportType.IntelGPAExportFull;
+    }
+
+    if (entries.length === 1 && entries[0].path.endsWith('.obj')) {
+      return ImportType.IntelGPAExportPartial;
+    }
+
+    return ImportType.Unknown;
+  }
+
+  /**
+   * Updates
+   * @param dropzone
+   * @param file
+   */
+  private updateIntelGPADropzone(dropzone: IntelGPADropzone, file: File) {
+    this.intelGPADropzones[dropzone].dropzone.innerHTML = file.name;
+    this.intelGPADropzones[dropzone].dropzone.classList.add('has-file');
+  }
+
+  /**
+   *
+   */
+  private async processIntelGPAImport() {
+    // bail out unless all files are present
+    for (const importTarget in this.intelGPAImports) {
+      if (!this.intelGPAImports[importTarget]) {
+        return;
+      }
+    }
+
+    const fileRoles = await identifyIntelGPAImport(
+      this.intelGPAImports['landmark-source']!,
+      this.intelGPAImports['landmark-output']!,
+      this.intelGPAImports['scene']!
+    );
+
+    if (!fileRoles) {
+      // TODO: throw an error or something
+      return;
+    }
+
+    alert('TODO: implement loading');
+  }
+
+  private async handleFiles(entries: FileInfo[], target?: IntelGPADropzone | 'renderdoc-export'): Promise<void> {
+    console.log('handling files', entries, target);
+
+    if (entries.length === 0) {
+      return;
+    }
+
+    const importType = await this.identifyImportType(entries);
+
+    if (importType === ImportType.Unknown) {
+      this.setStatus("Unknown import type");
+      return;
+    }
+
+    if (importType === ImportType.IntelGPAExportPartial) {
+      if (target && target !== 'renderdoc-export') {
+        this.intelGPAImports[target] = entries[0].file;
+        this.updateIntelGPADropzone(target as IntelGPADropzone, entries[0].file);
+      } else {
+        const guessedTarget = guessIntelGPAImportTargetFromFilename(entries[0]);
+        if (guessedTarget) {
+          this.intelGPAImports[guessedTarget] = entries[0].file;
+          this.updateIntelGPADropzone(guessedTarget as IntelGPADropzone, entries[0].file);
+        }
+      }
+      this.processIntelGPAImport();
+      return;
+    }
+
+    if (importType === ImportType.IntelGPAExportFull) {
+      const guessedTargets = guessIntelGPAImportTargetsFromFilenames(entries);
+      for (const key in guessedTargets) {
+        this.intelGPAImports[key] = guessedTargets[key];
+        this.updateIntelGPADropzone(key as IntelGPADropzone, guessedTargets[key].file);
+      }
+      this.processIntelGPAImport();
+      return;
+    }
+
 
     const vfs = new VirtualFileSystem();
     for (const { path, file } of entries) vfs.set(path, file);
