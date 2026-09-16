2cd970430d0331e6b5a609d79f401360b241f0fb
Tamius Han <ziga.pirih@hotmail.com>
2026-09-16 21:57:17 +0200

    Register new web components

--- src/components/wcmodule.ts
+++ src/components/wcmodule.ts
@@ -1,7 +1,15 @@
-import { CaptureImporter } from "./capture-importer/cls.capture-importer";
-import { RenderPassList } from "./common/cmp.render-pass-list";
+import { CaptureImporter } from "./capture-importer/cmp.capture-importer";
+import { RenderPassList } from "./common/render-pass/cmp.render-pass-list";
+import { LoadingScreen } from "./loading-screen/cmp.loading-screen";
+import { Overlay } from "./common/overlay/cmp.overlay";
+import { ControlScheme } from "./control-scheme/cmp.control-scheme";
+import { ExportMesh } from './export-mesh/cmp.export-mesh';
 
 export function registerWebComponents() {
   customElements.define("capture-importer", CaptureImporter);
   customElements.define("render-pass-list", RenderPassList);
+  customElements.define("loading-screen", LoadingScreen);
+  customElements.define("overlay-component", Overlay);
+  customElements.define("control-scheme", ControlScheme);
+  customElements.define("export-mesh", ExportMesh);
 }
