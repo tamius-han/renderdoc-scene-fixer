e6caba0474ad7bab1a37b972672425f57c7e0109
Tamius Han <ziga.pirih@hotmail.com>
2026-09-22 23:15:46 +0200

    Prepare deployment for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "./",
+  base: "/renderdoc-scene-fixer/",
   build: {
     target: "es2020",
   },
